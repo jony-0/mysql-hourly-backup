53723c842637e571919f6585df420e71363433e4
jony-0 <22551552+jony-0@users.noreply.github.com>
2022-02-10 10:26:44 -0500

    Delete backup.sh

--- backup.sh
+++
@@ -1,7 +0,0 @@
-backedup_db_name=`date +"%Y-%m-%d-%H"-db-name.gz`
-database_name='enter_db_name_here'
-s3_bucket_name='enter_bucket_name_here'
-my_cnf_path='enter_path_to_.my.cnf_file_here/.my.cnf'
-
-mysqldump --defaults-file=$my_cnf_path $database_name | gzip > $backedup_db_name
-aws s3 mv $backedup_db_name s3://$s3_bucket_name/mysql-backup/
